5401da5bc09873a11745aaea5d0cd02cfadb3542
Jennifer Minnich <jminniecc@gmail.com>
2017-01-09 15:14:34 -0700

    commit idex.html 2nd revision

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>	QUAY.cncpts	</title>
+			<link href="" rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div class="header">
+			<h1>Who is Quay.cnpts?</h1>
+		</div>
+		<ul>
+			<li>
+				<h2>Artist Statement</h2>
+				<p>I started QUAY.cncpts in 2015 to separate myself from my mass market (sometimes corporate) product design industry
+					past. I wanted to take a year and create a solo body of work that fundamentally
+					changed the perspective of a consumer driven product (in this case: apparel) by challenging
+					the definitive category and end use.</p>
+			</li>
+			<li>
+			<h3>MY APPROACH:</h3>
+				<p>
+					I began by creating fabric sculptures in muslin, an unattractive canvas material
+					typically used in the industry to mock up new styles and be thrown away after.
+					Purposely drawing focus to a futile process within the industry, I aimed to treat
+					this disposable fabrication in opposition: as a specialty, affected in different
+					ways to appear more expensive, valuable, and influential.</p>
+					<br>
+					<p>
+						To continue my experiment of how we identify objects, I developed a series of
+					garments in clear vinyl. This fabric completely disrupted the style’s intended
+					application, especially when mounted on canvas and serving as a multidimensional
+					composition. Directly drawing on the relationship between objects,
+					how they are presented, and how they are identified, I intend to give the
+					observer the opportunity to define each piece on their own by drawing from
+						their own experiences.</p>
+			</li>
+			<li>
+				<h3>MY MISSION:</h3>
+					<p>I strive to expand perspective- specifically, the connection between objects
+					and identity. While I have started with a small collection of apparel examples (a
+					familiar product and overlooked resource), my goal and essentially the
+					explanation of my work is to ultimately continue to challenge what identity is,
+					why, and the assumption of definitive categories.</p>
+			</li>
+		</ul>
+	</body>
+</html>
