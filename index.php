71cbaa84f9eb95e94c3b95c4e03561c71f7ff6e8
Dante Conley <danteconley@icloud.com>
2017-01-10 09:36:23 -0700

    first edit

--- index.php
+++ index.php
@@ -11,23 +11,22 @@
 		<br>
 		<br>
 		<h2>Artist Statement</h2>
-		<p>I started QUAY.cncpts in 2015 to separate myself from my mass market (sometimes corporate) product design industry
-			past. I wanted to take a year and create a solo body of work that fundamentally
-			changed the perspective of a consumer driven product (in this case: apparel) by challenging
+		<p>I started QUAY.cncpts in 2015 to separate myself from the mass market and sometimes corporate product design industry
+			I had worked in. I wanted to take a year, and create a solo body of work that fundamentally
+			changed the perspective of a consumer driven product (i.e. apparel), by challenging
 			the definitive category and end use.</p>
-		<br>
 		<h3>MY APPROACH:</h3>
 		<p>
 			I began by creating fabric sculptures in muslin, an unattractive canvas material
 			typically used in the industry to mock up new styles and be thrown away after.
-			Purposely drawing focus to a futile process within the industry, I aimed to treat
-			this disposable fabrication in opposition: as a specialty, affected in different
+			Purposefully drawing focus to a futile and wasteful process within the industry.
+			I aimed to shed new light on this fabric; as a specialty, affected in different
 			ways to appear more expensive, valuable, and influential.</p>
 		<p>
 			To continue my experiment of how we identify objects, I developed a series of
 			garments in clear vinyl. This fabric completely disrupted the style’s intended
 			application, especially when mounted on canvas and serving as a multidimensional
-			composition. Directly drawing on the relationship between objects,
+			composition. Drawing diretly on the relationship between objects,
 			how they are presented, and how they are identified, I intend to give the
 			observer the opportunity to define each piece on their own by drawing from
 			their own experiences.</p>
@@ -37,6 +36,5 @@
 			familiar product and overlooked resource), my goal and essentially the
 			explanation of my work is to ultimately continue to challenge what identity is,
 			why, and the assumption of definitive categories.</p>
-
 	</body>
 </html>
